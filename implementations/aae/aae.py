276e8dd24d2301e4820f403365ea1378115360df
Laura <lauragibbs123@googlemail.com>
2021-04-10 16:00:30 +0100

    csdataset class and aae.py adapted

--- implementations/aae/aae.py
+++ implementations/aae/aae.py
@@ -15,6 +15,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+from csdataset import CSDataset
+
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
@@ -124,13 +126,20 @@ if cuda:
 # Configure data loader
 os.makedirs("../../data/mnist", exist_ok=True)
 dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "../../data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
+    # datasets.MNIST(
+    #     "../../data/mnist",
+    #     train=True,
+    #     download=True,
+    #     transform=transforms.Compose(
+    #         [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+    #     ),
+    # ),
+    CSDataset(
+        root_dir = '../MDT-Calculations/saved_tiles' + '/training/tiles',
+        transform=transforms.Compose([
+            transforms.Resize(64),
+            transforms.ToTensor(),
+        ])
     ),
     batch_size=opt.batch_size,
     shuffle=True,
